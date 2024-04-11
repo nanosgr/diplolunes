8ebb69a254151c2c4c2da9ab64bd0bc0ae8cca76
nanosgr <sebastian.g.rios@gmail.com>
2024-04-11 17:08:09 -0300

    Actualizamos ejercicio

--- m5u4/routes/admin/login.js
+++ m5u4/routes/admin/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('../../models/usuarios');
 
 router.get('/', function(req, res, next) {
   res.render('admin/login', 
@@ -7,5 +8,29 @@ router.get('/', function(req, res, next) {
         layout: 'admin/layout', 
     });
 });
+
+router.post('/', async function(req, res, next) {
+  try{
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+    var usuario = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+    if(usuario){
+        res.redirect('/admin/usuarios');
+    }else{
+        res.render('admin/login', 
+        {
+            layout: 'admin/layout', 
+            error: true
+        });
+    }}catch(error){
+        console.log(error);
+        res.render('admin/login', 
+        {
+            layout: 'admin/layout', 
+            error: true
+        });
+      } 
+  });
+
 
 module.exports = router;
